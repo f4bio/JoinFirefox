b776a089adbc7ce816da48d590b957c51e1e49c0
Fabio Tea <iam@f4b.io>
2019-08-20 18:13:06 +0200

    save

--- js/base/constants.js
+++ js/base/constants.js
@@ -1,3 +1,14 @@
 var Constants = {
-	"REPLY_DIRECTLY": "Reply Directly"
+	"REPLY_DIRECTLY": "Reply Directly",
+	"OAUTH2": {
+		"client_id": "596310809542-5tfd4b5u90jmh0r664mdl73cnct5m0ea.apps.googleusercontent.com",
+		"client_id_web": "596310809542-c2bg952rtmf05el5kouqlcf0ajqnfpdl.apps.googleusercontent.com",
+		"scopes": [
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+			"https://www.googleapis.com/auth/drive.appfolder",
+			"https://www.googleapis.com/auth/drive",
+			"https://www.googleapis.com/auth/drive.file"
+		]
+	}
 };
